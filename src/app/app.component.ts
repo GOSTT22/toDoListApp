80178884fa9c3bad844ba71167a2559bbc34197c
Anatolii <115033897+GOSTT22@users.noreply.github.com>
2024-04-05 12:13:41 +0300

    moove main lodgic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,64 +16,6 @@ import {ProgressSpinnerMode, MatProgressSpinnerModule} from '@angular/material/p
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  colorControl = new FormControl('primary' as ThemePalette);
-  title = 'app';
-  // color: ThemePalette = 'primary';
-  // mode: ProgressSpinnerMode = 'determinate';
-  // value = 50;
-
-  // Example
-
-  client: ClientInterface = {
-    _id: "11",
-    task_name: "Buldog",
-    description: "basketball team",
-    status: "Todo"
-  }
-
-  selectedType$: BehaviorSubject<string> = new BehaviorSubject('');
-
-  types$ = this.store.select(allTypesSelector).pipe(
-    tap(types => {
-      if (types) {
-        this.selectedType$.next(types[0])
-      }
-    })
-  );
-  clients$ = combineLatest([this.selectedType$, this.store.select(allClientsSelector)]).pipe(
-    map(([type, clients]) => {
-      return clients? clients.filter(client => client.status === type) : []
-    }),
-    tap(r => console.log(r))
-  );
-  error$ = this.store.select(ErrorSelector);
-
-  constructor(private store: Store, private router: Router) { }
-
   ngOnInit(): void {
-    this.store.dispatch(getAllClientsAction());
-    setTimeout(() => { this.store.dispatch(getTypesOfClients()); }, 100)
   }
-
-  selectedValue(type: string): void {
-    this.selectedType$.next(type);
-  }
-
-  toggle: boolean;
-  createClient(): void {
-    this.toggle = !this.toggle;
-    const initialClient: ClientInterface = {
-      _id: "0",
-      task_name: "",
-      description: "",
-      status: ""
-    };
-    this.store.dispatch(setSelectedClientAction({ client: initialClient }));
-    this.router.navigate(['add-edit-client']);
-    if (!this.toggle) {
-      this.router.navigate(['']);
-      // this.store.dispatch(clearSelectedClientAction());
-    }
-  }
-
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { clearSelectedClientAction, createClientAction, getAllClientsAction, getTypesOfClients, setSelectedClientAction } from '../../store/client.actions';
+import { ErrorSelector, allClientsSelector, allTypesSelector } from '../../store/client.selector';
+import { map, tap } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { ClientInterface } from '../../store/client.interface';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
+import {ProgressSpinnerMode, MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +16,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  colorControl = new FormControl('primary' as ThemePalette);
+  title = 'app';
+  // color: ThemePalette = 'primary';
+  // mode: ProgressSpinnerMode = 'determinate';
+  // value = 50;
 
-  constructor() { }
+  // Example
+
+  client: ClientInterface = {
+    _id: "11",
+    task_name: "Buldog",
+    description: "basketball team",
+    status: "Todo"
+  }
+
+  selectedType$: BehaviorSubject<string> = new BehaviorSubject('');
+
+  types$ = this.store.select(allTypesSelector).pipe(
+    tap(types => {
+      if (types) {
+        this.selectedType$.next(types[0])
+      }
+    })
+  );
+  clients$ = combineLatest([this.selectedType$, this.store.select(allClientsSelector)]).pipe(
+    map(([type, clients]) => {
+      return clients? clients.filter(client => client.status === type) : []
+    }),
+    tap(r => console.log(r))
+  );
+  error$ = this.store.select(ErrorSelector);
+
+  constructor(private store: Store, private router: Router) { }
 
   ngOnInit(): void {
+    this.store.dispatch(getAllClientsAction());
+    setTimeout(() => { this.store.dispatch(getTypesOfClients()); }, 100)
+  }
+
+  selectedValue(type: string): void {
+    this.selectedType$.next(type);
+  }
+
+  toggle: boolean;
+  createClient(): void {
+    this.toggle = !this.toggle;
+    const initialClient: ClientInterface = {
+      _id: "0",
+      task_name: "",
+      description: "",
+      status: ""
+    };
+    this.store.dispatch(setSelectedClientAction({ client: initialClient }));
+    this.router.navigate(['add-edit-client']);
+    if (!this.toggle) {
+      this.router.navigate(['']);
+      // this.store.dispatch(clearSelectedClientAction());
+    }
   }
 
 }
